82366e210a54a0e86329be6b1dc5b9b8a1574ded
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-06-28 22:37:15 +0300

    feat: Add AbstractCommand to simplify text command creation

---
+++ src/Command/AbstractCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TelegramBot\Bundle\Command;
+
+use TelegramBot\Bundle\Client\TelegramClient;
+
+abstract class AbstractCommand implements CommandInterface
+{
+    public function __construct(
+        protected readonly TelegramClient $client
+    ) {}
+
+    /**
+     * Automatically extracts the chat ID from the update and sends a reply message.
+     */
+    protected function replyWithMessage(array $update, string $text, array $extraParams = []): array
+    {
+        $chatId = $update['message']['chat']['id'] ?? null;
+        
+        if (!$chatId) {
+            throw new \RuntimeException('Cannot reply: no chat_id found in the update.');
+        }
+
+        return $this->client->sendMessage($chatId, $text, $extraParams);
+    }
+}
