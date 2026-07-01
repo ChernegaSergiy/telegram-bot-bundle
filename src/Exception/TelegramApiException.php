54d8d1c0e4f3221c1d585a436d55ba685f09e4d6
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-01 19:07:39 +0300

    feat: Add TelegramApiException class

---
+++ src/Exception/TelegramApiException.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Morfeditorial\TelegramBotBundle\Exception;
+
+class TelegramApiException extends \RuntimeException
+{
+    private int $telegramErrorCode;
+    private string $telegramDescription;
+
+    public function __construct(string $description, int $errorCode = 0, \Throwable $previous = null)
+    {
+        $this->telegramErrorCode = $errorCode;
+        $this->telegramDescription = $description;
+        
+        $message = sprintf('Telegram API Error %d: %s', $errorCode, $description);
+        parent::__construct($message, $errorCode, $previous);
+    }
+
+    public function getTelegramErrorCode(): int
+    {
+        return $this->telegramErrorCode;
+    }
+
+    public function getTelegramDescription(): string
+    {
+        return $this->telegramDescription;
+    }
+
+    /**
+     * Telegram does not provide a specific error code for "message is not modified"
+     * (it just returns 400 Bad Request). We must check the description string.
+     */
+    public function isMessageNotModified(): bool
+    {
+        return $this->telegramErrorCode === 400 && str_contains(strtolower($this->telegramDescription), 'message is not modified');
+    }
+}
